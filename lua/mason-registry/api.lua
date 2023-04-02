434dac62cf44689c776bef8e8a6e15528cff8225
Brandon Capener <bcapener@micron.com>
2023-04-02 16:03:10 -0600

    update BASE_URL

--- lua/mason-registry/api.lua
+++ lua/mason-registry/api.lua
@@ -4,7 +4,8 @@ local fetch = require "mason-core.fetch"
 local api = {}
 
 -- https://github.com/williamboman/mason-registry-api
-local BASE_URL = "https://api.mason-registry.dev"
+-- local BASE_URL = "https://api.mason-registry.dev"
+local BASE_URL = "https://github.com/williamboman/mason-registry-api"
 
 local stringify_params = _.compose(_.join "&", _.map(_.join "="), _.sort_by(_.head), _.to_pairs)
 
